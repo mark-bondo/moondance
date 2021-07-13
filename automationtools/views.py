17df70d4ff8768e27cc40fae4105228f4759bd79
Mark Bondo <bondo.mark@gmail.com>
2021-07-13 14:49:33 -0400

    testing webhook

--- automationtools/views.py
+++ automationtools/views.py
@@ -1,11 +1,19 @@
-from integration.models import Shopify_Product
 import json
+from django.db import connection
+from integration.models import Shopify_Product
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def product_hook(request, action):
-    item = json.loads(request.body)
+    # item = json.loads(request.body)
 
-    if item.action == "delete":
-        p = Shopify_Product.objects.get(id=item.id)
-        p._active = False
-        p.save()
+    with open("test.json", "w") as w:
+        w.write(request.body)
+
+    # if item.action == "delete":
+    #     products = Shopify_Product.objects.filter(id=item.id)
+
+    #     for p in products:
+    #         p._active = False
+    #         p.save()
