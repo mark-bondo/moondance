04e91e5051107da55bd763db78f45d6d1c30021f
Mark Bondo <bondo.mark@gmail.com>
2022-02-06 12:49:11 -0500

    updates for costs

--- purchasing/models.py
+++ purchasing/models.py
@@ -7,10 +7,18 @@ from django.utils import timezone
 
 LOCATION_LIST = [
     ("Bondo - Garage", "Bondo - Garage"),
-    ("Bondo - 2nd Floor", "Bondo - 2nd Floor"),
-    ("MoonDance - Workshop", "MoonDance - Workshop"),
-    ("MoonDance - Fulfillment Center", "MoonDance - Fulfillment Center"),
+    ("Workshop", "Workshop"),
+    ("Fulfillment Area", "Fulfillment Area"),
+    ("DFM Staging", "DFM Staging"),
+    ("Curing Room", "Curing Room"),
+    ("WomanCraft", "WomanCraft"),
+    ("FBA", "FBA"),
+    ("Offsite Wrapping", "Offsite Wrapping"),
 ]
+TRANSACTION_TYPES = (
+    ("Opening Balance", "Opening Balance"),
+    ("Closing Balance", "Closing Balance"),
+)
 
 
 class Supplier(MetaModel):
@@ -21,9 +29,7 @@ class Supplier(MetaModel):
 
     history = HistoricalRecords()
 
-    type = models.CharField(
-        max_length=200, choices=type_choices, default="Manufacturer"
-    )
+    type = models.CharField(max_length=200, choices=type_choices, default="Manufacturer")
     name = models.CharField(max_length=200, unique=True)
     contact_name = models.CharField(max_length=200, null=True, blank=True)
     contact_email = models.CharField(max_length=200, null=True, blank=True)
@@ -31,9 +37,7 @@ class Supplier(MetaModel):
     city = models.CharField(max_length=200, null=True, blank=True)
     state = models.CharField(max_length=200, null=True, blank=True)
     postal_code = models.CharField(max_length=200, null=True, blank=True)
-    country = models.CharField(
-        max_length=200, null=True, blank=True, default="United States"
-    )
+    country = models.CharField(max_length=200, null=True, blank=True, default="United States")
     supplier_website = models.URLField(max_length=200, null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
     phone_number = models.CharField(max_length=50, null=True, blank=True)
@@ -55,9 +59,7 @@ class Supplier_Product(MetaModel):
         on_delete=models.PROTECT,
         related_name="Supplier_Product_product_fk",
     )
-    supplier = models.ForeignKey(
-        Supplier, on_delete=models.PROTECT, related_name="supplier_product_supplier_fk"
-    )
+    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, related_name="supplier_product_supplier_fk")
     supplier_sku = models.CharField(max_length=200)
     supplier_sku_description = models.CharField(max_length=200)
     supplier_sku_link = models.URLField(max_length=200, null=True, blank=True)
@@ -83,9 +85,8 @@ class Invoice(MetaModel):
     invoice = models.CharField(max_length=200, blank=True, null=True)
     order = models.CharField(max_length=200, blank=True, null=True)
     date_invoiced = models.DateField(default=timezone.now)
-    freight_charges = models.DecimalField(
-        max_digits=12, decimal_places=2, null=True, blank=True
-    )
+    freight_charges = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    invoice_attachment = models.FileField(null=True, blank=True)
 
     def __str__(self):
         return "{} ({})".format(self.invoice, self.supplier)
@@ -98,9 +99,7 @@ class Invoice(MetaModel):
 
 class Invoice_Line(MetaModel):
     history = HistoricalRecords()
-    invoice = models.ForeignKey(
-        Invoice, on_delete=models.CASCADE, related_name="Invoice_Line_invoice_fk"
-    )
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name="Invoice_Line_invoice_fk")
     sku = models.ForeignKey(
         Product,
         on_delete=models.PROTECT,
@@ -143,9 +142,7 @@ class Inventory_Onhand(MetaModel):
         on_delete=models.PROTECT,
         related_name="Inventory_Onhand_sku_fk",
     )
-    location = models.CharField(
-        max_length=200, choices=LOCATION_LIST, verbose_name="Current Location"
-    )
+    location = models.CharField(max_length=200, choices=LOCATION_LIST, verbose_name="Current Location")
     quantity_onhand = models.DecimalField(max_digits=12, decimal_places=2)
     to_location = models.CharField(
         max_length=200,
@@ -154,9 +151,7 @@ class Inventory_Onhand(MetaModel):
         choices=LOCATION_LIST,
         verbose_name="Transfer To Location",
     )
-    transfer_quantity = models.DecimalField(
-        max_digits=12, decimal_places=2, null=True, blank=True
-    )
+    transfer_quantity = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return "{} ({})".format(self.sku.sku, self.location)
@@ -171,3 +166,18 @@ class Inventory_Onhand(MetaModel):
                 "location",
             ),
         )
+
+
+class Item_Transaction_History(MetaModel):
+    history = HistoricalRecords()
+
+    sku = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT,
+        related_name="Item_Transaction_History_sku_fk",
+    )
+    transaction_type = models.CharField(max_length=200, choices=TRANSACTION_TYPES)
+    transaction_date = models.DateField()
+    location = models.CharField(max_length=200, choices=LOCATION_LIST)
+    quantity = models.DecimalField(max_digits=12, decimal_places=2)
+    unit_of_measure = models.CharField(max_length=200, choices=UNIT_OF_MEASURES)
